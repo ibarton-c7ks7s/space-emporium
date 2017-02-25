2ed156d353d7759cd3c797d944c7748733eac22c
Josh Coody <joshua.n.coody@gmail.com>
2017-02-25 16:16:39 -0500

    Setting up barebones of the project.
    Quickly loading in the API and connecting images. Will need to tweak image sizes. Still need to add functionality and styling.

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+
+  entry: './index.jsx',
+
+  output: {
+    path: '/public',
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+
+  plugins: process.env.NODE_ENV === 'production' ? [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin()
+  ] : []
+
+}
+
+module.exports = config;
